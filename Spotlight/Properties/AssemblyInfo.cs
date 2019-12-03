1666c415631bc7b8e170493366299d1bf2761d88
Ferose khan, Jeberla <j.ferosekhan@siemens-healthineers.com>
2019-12-03 15:10:19 +0530

    Added assembly info.

--- Spotlight/Properties/AssemblyInfo.cs
+++ Spotlight/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Spotlight")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple tool to show spotlight in PC.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ferose Khan J")]
 [assembly: AssemblyProduct("Spotlight")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
